7419fa52f79350bf7d6a077f5c29a52fcbfdf55a
GamaRL <gamarl1973@gmail.com>
2022-01-06 18:47:30 -0600

    REFACTOR: Se movió el cálculo de estadísticas a métodos

--- src/gui/vistas/Ventas.java
+++ src/gui/vistas/Ventas.java
@@ -2,12 +2,10 @@ package gui.vistas;
 
 import java.awt.Dimension;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.Box;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
@@ -19,16 +17,25 @@ import modelos.usuarios.Usuario;
 import repositorio.RepositorioUsuarios;
 
 /**
- * Clase que tiene los métodos correspondientes para mostrar las ventas realizadas en el restaurante 
- * Hereda de JPanel para uitlizar los métodos/funcionalidades de una vista gráfica
+ * Clase que tiene los métodos correspondientes para mostrar las ventas
+ * realizadas en el restaurante
+ * Hereda de JPanel para uitlizar los métodos/funcionalidades de una vista
+ * gráfica
  */
 public class Ventas extends JTabbedPane {
-//Atributo de tipo restaurante 
+  // Atributo de tipo restaurante
   private Restaurante restaurante;
-/**
- * Método constructor 
- * @param restaurante para obtener sus características 
- */
+
+  /**
+   * 
+   */
+  private double total;
+
+  /**
+   * Método constructor
+   * 
+   * @param restaurante para obtener sus características
+   */
   public Ventas(Restaurante restaurante) {
     super();
 
@@ -36,22 +43,13 @@ public class Ventas extends JTabbedPane {
 
     crearComponentes();
   }
-  /**
-   * Método que realiza la creación de los componentes y características que tiene la vista gráfica
-   * 
-   */
-  public void crearComponentes() {
 
-    removeAll();
-
-    Box contenedorGrafica = Box.createHorizontalBox();
-
+  public Map<Usuario, Double> getEstadisticasUsuarios() {
     Map<Usuario, Double> ventasMesero = new LinkedHashMap<>();
-    Map<Integer, Double> ventasMesa = new LinkedHashMap<>();
 
     List<Usuario> usuarios = RepositorioUsuarios.getUsuarios();
 
-    double total = 0;
+    total = 0;
 
     for (Usuario u : usuarios) {
       ventasMesero.put(u, 0.0);
@@ -65,33 +63,15 @@ public class Ventas extends JTabbedPane {
       total += avg;
     }
 
-    JTable tablaMeseros = new JTable();
-    DefaultTableModel modeloMeseros = new DefaultTableModel() {
-      @Override
-      public boolean isCellEditable(int row, int column) {
-        return false;
-      }
-    };
+    return ventasMesero;
+  }
 
-    tablaMeseros.setModel(modeloMeseros);
-    modeloMeseros.setColumnIdentifiers(new String[]{"Mesero", "Ventas"});
+  public Map<Integer, Double> getEstadisticasMesas() {
+    Map<Integer, Double> ventasMesa = new LinkedHashMap<>();
 
-    DecimalFormat formatter = new DecimalFormat("##.## %");
+    int numMesas = restaurante.getMesas().size();
 
-    for (Usuario u : usuarios) {
-      Object[] data = { u.getNombre(), formatter.format(ventasMesero.get(u) / total)};
-      modeloMeseros.addRow(data);
-    }
-
-    modeloMeseros.addRow(new Object[] {"Total", new DecimalFormat("$ ##.##").format(total)});
-
-    JScrollPane panelMeseros = new JScrollPane(tablaMeseros);
-    panelMeseros.setPreferredSize(new Dimension(500, 200));
-
-
-    int numMesas = restaurante.getMesas().size();    
-
-    for ( int i = 1; i <= numMesas; i++ ) {
+    for (int i = 1; i <= numMesas; i++) {
       ventasMesa.put(i, 0.0);
     }
 
@@ -101,7 +81,45 @@ public class Ventas extends JTabbedPane {
 
       ventasMesa.put(numMesa, avg);
     }
-    
+
+    return ventasMesa;
+  }
+
+  /**
+   * Método que realiza la creación de los componentes y características que tiene
+   * la vista gráfica
+   */
+  public void crearComponentes() {
+
+    removeAll();
+
+    Map<Usuario, Double> ventasMesero = getEstadisticasUsuarios();
+    Map<Integer, Double> ventasMesa = getEstadisticasMesas();
+
+    JTable tablaMeseros = new JTable();
+    DefaultTableModel modeloMeseros = new DefaultTableModel() {
+      @Override
+      public boolean isCellEditable(int row, int column) {
+        return false;
+      }
+    };
+
+    tablaMeseros.setModel(modeloMeseros);
+    modeloMeseros.setColumnIdentifiers(new String[] { "Mesero", "Ventas" });
+
+    DecimalFormat formatter = new DecimalFormat("##.## %");
+
+    for (Usuario u : ventasMesero.keySet()) {
+      Object[] data = { u.getNombre(), formatter.format(ventasMesero.get(u) / total) };
+      modeloMeseros.addRow(data);
+    }
+
+    modeloMeseros.addRow(new Object[] { "Total", new DecimalFormat("$ ##.##").format(total) });
+
+    JScrollPane panelMeseros = new JScrollPane(tablaMeseros);
+    panelMeseros.setPreferredSize(new Dimension(500, 200));
+
+
     JTable tablaMesas = new JTable();
     DefaultTableModel modeloMesas = new DefaultTableModel() {
       @Override
@@ -111,15 +129,15 @@ public class Ventas extends JTabbedPane {
     };
 
     tablaMesas.setModel(modeloMesas);
-    modeloMesas.setColumnIdentifiers(new String[]{"Mesa", "Ventas"});
+    modeloMesas.setColumnIdentifiers(new String[] { "Mesa", "Ventas" });
 
-    for ( int i = 1; i <= numMesas; i++ ) {
-      Object[] data = { String.format("Mesa %d", i), formatter.format(ventasMesa.get(i) / total)};
+    for (int i = 1; i <= ventasMesa.size(); i++) {
+      Object[] data = { String.format("Mesa %d", i), formatter.format(ventasMesa.get(i) / total) };
       modeloMesas.addRow(data);
     }
 
-    modeloMesas.addRow(new Object[] {"Total", new DecimalFormat("$ ##.##").format(total)});
-    
+    modeloMesas.addRow(new Object[] { "Total", new DecimalFormat("$ ##.##").format(total) });
+
     JScrollPane panelMesas = new JScrollPane(tablaMesas);
     panelMesas.setPreferredSize(new Dimension(500, 200));
 
